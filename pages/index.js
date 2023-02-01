63baefd5537bd2e090498f51f4bf1bc65bdab940
Evan Wagner <36539810+EvanWagner2@users.noreply.github.com>
2023-02-01 00:54:11 -0800

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import Header from '../components/header';
+
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>DonsBook</title>
+        <script src="https://kit.fontawesome.com/5de23f1946.js" crossorigin="anonymous"></script>
+        <link href="/dist/output.css" rel="stylesheet"></link>
+        </Head>
+ 
+        {/* Header */}
+         <Header>\
+        
+    </Header>
+        <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+        </main>
+  
+        </div>
+  );
+}
+
+
+
